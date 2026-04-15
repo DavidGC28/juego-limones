6562889d4f38d07d8b5f162b294ce98321a569e8
DavidGC28 <davicho9913@gmail.com>
2026-04-14 21:32:23 -0500

    agregar suelo y diseño de personaje

--- limones.js
+++ limones.js
@@ -1,1 +1,55 @@
+ let canvas=document.getElementById("areaJuego")
+ let ctx=canvas.getContext("2d")
+
+ function iniciarJuego(){
+    dibujarSuelo()
+    dibujarPersonaje()  
+ }
+
+ const ALTURA_SUELO = 40;
+ const ALTURA_PERSONAJE = 60; 
+ const ANCHO_PERSONAJE = 60;
+ const anchoRectangulo = 100; 
+  const altoRectangulo = 40;  
+
+function dibujarSuelo() {
+  const altoSuelo = ALTURA_SUELO; 
+  const ySuelo = canvas.height - altoSuelo;
+  const ancho = canvas.width;
+
  
+  ctx.fillStyle = "#5d4037"; // Marrón oscuro
+  ctx.fillRect(0, ySuelo, ancho, altoSuelo);
+
+
+  ctx.fillStyle = "#78d62c"; 
+  ctx.fillRect(0, ySuelo, ancho, 12); 
+
+ 
+  ctx.fillStyle = "#a2e06d";
+  ctx.fillRect(0, ySuelo, ancho, 4);
+
+  
+  for (let i = 0; i < ancho; i += 32) {
+   
+    ctx.fillStyle = "#49a620";
+    ctx.fillRect(i + 8, ySuelo + 8, 4, 4);
+    
+    
+    ctx.fillStyle = "#8d6e63";
+    ctx.fillRect(i + 20, ySuelo + 24, 4, 4);
+    
+    ctx.fillStyle = "#d7ccc8";
+    ctx.fillRect(i + 24, ySuelo + 24, 2, 2);
+  }
+}
+
+function dibujarPersonaje() {
+  const x = 400; 
+  const y = canvas.height - 60;
+    ctx.fillStyle = "#9c0909"; 
+    ctx.fillRect(canvas.width/2 - 50,canvas.height -100, 60, 60);
+    ctx.fillStyle = "#9c0909"; 
+    ctx.fillRect(canvas.width/2 -100,canvas.height -80, 100, 40);
+
+}
